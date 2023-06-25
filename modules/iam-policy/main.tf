77311bf8633fd493a9317d96efe393662332b374
DJ Lee <moovinzoo@gmail.com>
2023-06-25 16:43:39 +0900

    add: policy on s3, dynamodb for managing remote state files

--- modules/iam-policy/main.tf
+++ modules/iam-policy/main.tf
@@ -7,14 +7,21 @@ resource "aws_iam_policy" "ec2_fa_policy" {
   name        = "ec2_fa_policy"
   description = "Allows FullAccess for VPC management"
 
-  policy = data.aws_iam_policy_document.ec2_full-access_policy.json
+  policy = data.aws_iam_policy_document.ec2_fa_policy.json
 }
 
 resource "aws_iam_policy" "s3_fa_policy" {
   name        = "s3_fa_policy"
-  description = "Allows FullAccess for VPC management"
+  description = "Allows FullAccess for remote backend management"
 
-  policy = data.aws_iam_policy_document.s3_full-access_policy.json
+  policy = data.aws_iam_policy_document.s3_fa_policy.json
+}
+
+resource "aws_iam_policy" "dynamodb_fa_policy" {
+  name        = "dynamodb_fa_policy"
+  description = "Allows FullAccess for remote backend management"
+
+  policy = data.aws_iam_policy_document.dynamodb_fa_policy.json
 }
 
 # Attach policies to current user
@@ -26,4 +33,9 @@ resource "aws_iam_user_policy_attachment" "user_ec2-fa-policy_attachment" {
 resource "aws_iam_user_policy_attachment" "user_s3-fa-policy_attachment" {
   user       = data.aws_iam_user.current.user_name
   policy_arn = aws_iam_policy.s3_fa_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "user_dynamodb-fa-policy_attachment" {
+  user       = data.aws_iam_user.current.user_name
+  policy_arn = aws_iam_policy.dynamodb_fa_policy.arn
 }
