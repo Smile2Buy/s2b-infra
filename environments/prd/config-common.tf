fef2beb8c60d52861f0e5696cfb924ac0fed755e
DJ Lee <moovinzoo@gmail.com>
2023-06-29 00:54:32 +0900

    add: region with common configs

---
+++ environments/prd/config-common.tf
@@ -0,0 +1,7 @@
+locals {
+  common = {
+    organization = "s2b"
+    aws_region   = "ap-northeast-2"
+    environment  = "prd"
+  }
+}
---
+++ environments/prd/main.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  region = local.common.aws_region
+}
