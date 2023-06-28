cd8fba194c9e57093fa366528ba78fdea456133c
DJ Lee <moovinzoo@gmail.com>
2023-06-29 01:01:20 +0900

    add: network-layer with defining tag-name-prefix

---
+++ layers/network/variables.tf
@@ -0,0 +1,9 @@
+variable "organization" {
+  type        = string
+  description = "organization name"
+}
+
+variable "environment" {
+  type        = string
+  description = "environment of CI/CD pipeline"
+}
