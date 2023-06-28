4ae0c03aedb3ec784186c8f6bb527b1d28cede93
DJ Lee <moovinzoo@gmail.com>
2023-06-29 02:38:26 +0900

    add: route-table configurations with defining internet gateway resource

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -12,3 +12,15 @@ variable "subnet_config" {
   })
   description = "Subnet configs that is compatible with public/private"
 }
+
+variable "route_table_config" {
+  type = object({
+    name  = string
+    route = object({
+      cidr_block     = string
+      gateway_id     = string
+      nat_gateway_id = string
+    })
+  })
+  description = "Route-table configs for subnet"
+}
