afc46e42165b6cb6dab99269abe2d89a0656efcd
DJ Lee <moovinzoo@gmail.com>
2023-06-29 01:10:53 +0900

    add: network layer with weaved configs

---
+++ environments/prd/config-network.tf
@@ -0,0 +1,14 @@
+locals {
+  network = {
+    vpc = {
+      cidr_block = "10.0.0.0/16"
+      subnets    = [
+        {
+          availability_zone  = "ap-northeast-2a"
+          public_cidr_block  = "10.0.101.0/16"
+          private_cidr_block = "10.0.1.0/16"
+        }
+      ]
+    }
+  }
+}
--- environments/prd/main.tf
+++ environments/prd/main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = local.common.aws_region
 }
+
+module "network" {
+  source       = "../../layers/network"
+  organization = local.common.organization
+  environment  = local.common.environment
+  config       = local.network
+}
