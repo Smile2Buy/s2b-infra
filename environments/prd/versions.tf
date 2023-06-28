006881d7682c83103c743c09f8455342060ef07c
DJ Lee <moovinzoo@gmail.com>
2023-06-29 01:30:21 +0900

    modify: aws version to be compatible

--- environments/prd/versions.tf
+++ environments/prd/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.3.3"
+  required_version = ">= 1.3.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.15.1"
+      version = ">= 4.15.1"
     }
   }
 }
