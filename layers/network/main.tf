a3d2973809d3e8a1faca1de631b1f961b7c54256
DJ Lee <moovinzoo@gmail.com>
2023-06-29 01:10:53 +0900

    add: network layer with weaved configs

--- layers/network/main.tf
+++ layers/network/main.tf
@@ -2,3 +2,12 @@ locals {
   tag_name_prefix = "${var.organization}-${var.environment}"
   vpc_name        = local.tag_name_prefix
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.config.vpc.cidr_block
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = local.vpc_name
+  }
+}
--- layers/network/variables.tf
+++ layers/network/variables.tf
@@ -7,3 +7,19 @@ variable "environment" {
   type        = string
   description = "environment of CI/CD pipeline"
 }
+
+variable "config" {
+  type = object({
+    vpc = object({
+      cidr_block = string
+      subnets    = list(
+        object({
+          availability_zone  = string
+          public_cidr_block  = string
+          private_cidr_block = string
+        })
+      )
+    })
+  })
+  description = "configs for network-layer"
+}
