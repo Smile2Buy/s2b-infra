3af67cb23ea5a72d11830271025386554c742d01
DJ Lee <moovinzoo@gmail.com>
2023-06-29 00:50:20 +0900

    backup: existing modules [deleted]

--- modules/setup-vpc/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "vpc-sb2-ap-northeast-2" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "VPC: s2b-ap-northeast-2"
-  }
-}
--- modules/setup-vpc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vpc_cidr" {
-  type        = string
-  description = "Public subnet CIDR values"
-  default     = "10.0.0.0/16"
-}
