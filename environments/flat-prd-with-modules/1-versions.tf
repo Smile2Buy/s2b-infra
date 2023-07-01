161304739ca46cd2f10a96dcca19382ae3e85638
DJ Lee <moovinzoo@gmail.com>
2023-07-01 09:47:43 +0900

    add: latest version kubernetes provider with args & command

--- environments/flat-prd-with-modules/1-versions.tf
+++ environments/flat-prd-with-modules/1-versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.6.2"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.21.1"
+    }
   }
 }
