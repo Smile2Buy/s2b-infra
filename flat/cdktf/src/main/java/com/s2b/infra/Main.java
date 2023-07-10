c154e86b8bb1b6ad18a0277c104490a7caa8eec7
DJ Lee <moovinzoo@gmail.com>
2023-07-10 23:49:50 +0900

    backup: -

--- flat/cdktf/src/main/java/com/s2b/infra/Main.java
+++ flat/cdktf/src/main/java/com/s2b/infra/Main.java
@@ -1,13 +1,17 @@
 package com.s2b.infra;
 
-import com.hashicorp.cdktf.App;
+import com.hashicorp.cdktf.*;
 
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         final App app = new App();
-        new MainStack(app, "cdktf");
+        TerraformStack stack = new MainStack(app, "cdktf");
+        new RemoteBackend(stack, RemoteBackendConfig.builder()
+                .hostname("app.terraform.io")
+                .organization("smile2buy")
+                .workspaces(new NamedRemoteWorkspace("cdktf"))
+                .build());
         app.synth();
     }
 }
--- flat/cdktf/src/main/java/com/s2b/infra/MainStack.java
+++ flat/cdktf/src/main/java/com/s2b/infra/MainStack.java
@@ -1,5 +1,8 @@
 package com.s2b.infra;
 
+import com.hashicorp.cdktf.TerraformOutput;
+import com.hashicorp.cdktf.providers.aws.instance.Instance;
+import com.hashicorp.cdktf.providers.aws.provider.AwsProvider;
 import software.constructs.Construct;
 
 import com.hashicorp.cdktf.TerraformStack;
@@ -9,6 +12,17 @@ public class MainStack extends TerraformStack
     public MainStack(final Construct scope, final String id) {
         super(scope, id);
 
-        // define resources here
+        AwsProvider.Builder.create(this, "AWS")
+                .region("ap-northeast-2")
+                .build();
+
+        Instance instance = Instance.Builder.create(this, "compute")
+                .ami("ami-00e172885f09be17c")
+                .instanceType("t3.micro")
+                .build();
+
+        TerraformOutput.Builder.create(this, "public_ip")
+                .value(instance.getPublicIp())
+                .build();
     }
 }
