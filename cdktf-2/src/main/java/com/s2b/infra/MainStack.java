d34926e34071763a851d0d2686b46f1b366620f2
moovinzoo <moovinzoo@gmail.com>
2023-07-13 21:25:30 +0900

    feat: add sample instance with output config

--- cdktf-2/src/main/java/com/s2b/infra/MainStack.java
+++ cdktf-2/src/main/java/com/s2b/infra/MainStack.java
@@ -1,6 +1,9 @@
 package com.s2b.infra;
 
+import com.hashicorp.cdktf.TerraformOutput;
 import com.hashicorp.cdktf.TerraformStack;
+import com.hashicorp.cdktf.providers.aws.instance.Instance;
+import com.hashicorp.cdktf.providers.aws.provider.AwsProvider;
 import software.constructs.Construct;
 
 public class MainStack extends TerraformStack {
@@ -8,5 +11,17 @@ public class MainStack extends TerraformStack {
         super(scope, id);
 
         // define resources here
+        AwsProvider.Builder.create(this, "AWS")
+                .region("ap-northeast-2")
+                .build();
+
+        Instance instance = Instance.Builder.create(this, "compute")
+                .ami("ami-00e172885f09be17c")
+                .instanceType("t3.micro")
+                .build();
+
+        TerraformOutput.Builder.create(this, "public_ip")
+                .value(instance.getPublicIp())
+                .build();
     }
 }
