fb7aaeb0fc98e1b450cbe1673ad78fe9018134a6
moovinzoo <moovinzoo@gmail.com>
2023-07-13 21:24:55 +0900

    feat: set cloud backend

--- cdktf-2/src/main/java/com/s2b/infra/Main.java
+++ cdktf-2/src/main/java/com/s2b/infra/Main.java
@@ -4,16 +4,19 @@ import com.hashicorp.cdktf.App;
 import com.hashicorp.cdktf.CloudBackend;
 import com.hashicorp.cdktf.CloudBackendConfig;
 import com.hashicorp.cdktf.NamedCloudWorkspace;
+import com.hashicorp.cdktf.TerraformStack;
 
 
 public class Main {
     public static void main(String[] args) {
         final App app = new App();
-        MainStack stack = new MainStack(app, "s2b-infra-simple");
+        TerraformStack stack = new MainStack(app, "s2b-infra-simple");
         new CloudBackend(stack, CloudBackendConfig.builder()
                 .hostname("app.terraform.io")
                 .organization("smile2buy")
-                .workspaces(new NamedCloudWorkspace("s2b-infra-bootstrap")).build());
+                .workspaces(new NamedCloudWorkspace("s2b-infra-bootstrap"))
+                .build());
+
         app.synth();
     }
 }
